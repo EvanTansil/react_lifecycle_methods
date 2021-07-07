98032e0db6d0dc98d96aead6c99a871e9b9aa472
Evan Tansil <etevan87@gmail.com>
2021-07-07 14:57:50 -0400

    lifecycle exercise

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,27 @@ import React, { Component } from "react";
 class App extends Component {
   constructor(props) {
     super(props);
+  this.state={text:"house",hasLoaded:false} 
+
   }
+  handleInput(e){
+          this.setState(text:e.target.value )
+  }
+
+handleClick(){
+this.setState({hasLoaded: ! this.state.hasLoaded})
+
+}
+
+
 
   render() {
     return (
       <div>
         <h1>Welcome to React!</h1>
         <hr />
+        <input type="text" aria-label="title" onChange={this.handleInput}/>
+      <button onclick={this.handleClick}></button>
       </div>
     );
   }
