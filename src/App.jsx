d84cf2ba0c1a944d676d0dc620261ac8aedf1a21
Evan Tansil <etevan87@gmail.com>
2021-09-24 11:59:23 -0400

    react_lifecycle_methods

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ class App extends Component {
   constructor(props) {
     super(props);
   this.state={text:"house",hasLoaded:false} 
-
-  }
+this.handleInput=this.handleInput.bind(this)
+this.handleClick=this.handleClick.bind (this)  
+}
   handleInput(e){
-          this.setState(text:e.target.value )
+          this.setState({text:e.target.value })
   }
 
 handleClick(){
@@ -18,14 +19,26 @@ this.setState({hasLoaded: ! this.state.hasLoaded})
 
 
   render() {
-    return (
-      <div>
-        <h1>Welcome to React!</h1>
-        <hr />
-        <input type="text" aria-label="title" onChange={this.handleInput}/>
-      <button onclick={this.handleClick}></button>
-      </div>
-    );
+    if(this.state.hasLoaded)
+  {return (
+    <div>
+    <h1>{this.state.text }</h1>
+    <hr />
+    <input type="text" aria-label="title" onChange={this.handleInput}/>
+  <button onClick={this.handleClick}>Unload </button>
+  </div>
+
+  )}
+
+    else 
+  {return  (
+    <div>
+      <h1>loading </h1>
+      <button onClick={this.handleClick}>Load</button>
+       </div>
+   )}
+
+    
   }
 }
 
